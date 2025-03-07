e0f91256df664da6423971db95ca20da0372528b
Silas Justino <105759554+silasjustino@users.noreply.github.com>
2025-03-07 19:16:27 -0300

    Produtos iniciais com melhores descrições
    Produtos iniciais com melhores descrições

--- src/main/java/controller/ListaProdutosServlet.java
+++ src/main/java/controller/ListaProdutosServlet.java
@@ -18,9 +18,9 @@ public class ListaProdutosServlet extends HttpServlet {
         List<Produto> produtos = (List<Produto>) request.getSession().getAttribute("listaProdutos");
         if (produtos == null) {
             produtos = new ArrayList<>();
-            produtos.add(new Produto("Notebook", "Notebook Gamer", 4500.00, 10));
-            produtos.add(new Produto("Mouse", "Mouse sem fio", 150.00, 25));
-            produtos.add(new Produto("Teclado", "Teclado mecânico", 350.00, 15));
+            produtos.add(new Produto("Notebook Acer 18.5\"", "Notebook Acer 18.5\" - 16GB de Memória RAM, 1TB de Memória SSDM2, Processador AMD Ryzen 7 7200X, Placa de vídeo NVIDIA GeForce 4090Ti", 8500.00, 10));
+            produtos.add(new Produto("Mouse Razer HLM-3", "Mouse sem fio Razer HLM-3 Modelo Gamer, com sensor de 20.000 DPI e switches ópticos, o Razer DeathAdder V2 oferece precisão e cliques rápidos. Seu design ergonômico e cabo Speedflex garantem conforto e movimentos suaves. Personalização total com RGB Chroma", 350.00, 25));
+            produtos.add(new Produto("Teclado Logitech G Pro X", "O Teclado mecânico apresenta switches mecânicos personalizáveis, permitindo uma experiência de digitação única. Com iluminação RGB LIGHTSYNC, oferece total personalização e performance de alto nível em um design compacto e durável, ideal para jogadores profissionais.", 275.00, 15));
             request.getSession().setAttribute("listaProdutos", produtos);
         }
         request.setAttribute("listaProdutos", produtos);
