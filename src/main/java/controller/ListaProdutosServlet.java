e0374f231d16c6e2b53ee1bd4667e6123f3ed990
Kadmiel T <132308058+KadmielT@users.noreply.github.com>
2025-05-08 22:59:56 -0300

    Tela de listagem

--- src/main/java/controller/ListaProdutosServlet.java
+++ src/main/java/controller/ListaProdutosServlet.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.Produto;
+import dao.ProdutoDAO;  // <- Importação do DAO
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,21 +9,24 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/listaProdutos")
 public class ListaProdutosServlet extends HttpServlet {
+    private ProdutoDAO produtoDAO;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        produtoDAO = new ProdutoDAO(); // Inicializa o DAO
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<Produto> produtos = (List<Produto>) request.getSession().getAttribute("listaProdutos");
-        if (produtos == null) {
-            produtos = new ArrayList<>();
-            produtos.add(new Produto("Notebook Acer 18.5", "Notebook Acer 18.5 - 16GB de Memória RAM, 1TB de Memória SSDM2, Processador AMD Ryzen 7 7200X, Placa de vídeo NVIDIA GeForce 4090Ti", 8500.00, 10));
-            produtos.add(new Produto("Mouse Razer HLM-3", "Mouse sem fio Razer HLM-3 Modelo Gamer, com sensor de 20.000 DPI e switches ópticos, o Razer DeathAdder V2 oferece precisão e cliques rápidos. Seu design ergonômico e cabo Speedflex garantem conforto e movimentos suaves. Personalização total com RGB Chroma", 350.00, 25));
-            produtos.add(new Produto("Teclado Logitech G Pro X", "O Teclado mecânico apresenta switches mecânicos personalizáveis, permitindo uma experiência de digitação única. Com iluminação RGB LIGHTSYNC, oferece total personalização e performance de alto nível em um design compacto e durável, ideal para jogadores profissionais.", 275.00, 15));
-            request.getSession().setAttribute("listaProdutos", produtos);
-        }
+        // Busca os produtos do banco de dados
+        List<Produto> produtos = produtoDAO.buscarTodos();
+
+        // Envia para a tela de listagem
         request.setAttribute("listaProdutos", produtos);
         request.getRequestDispatcher("listaProdutos.jsp").forward(request, response);
     }
